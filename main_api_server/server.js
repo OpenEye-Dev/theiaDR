e9808d2a6499f5a534ef32b21232550101ed92b2
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2017-02-17 12:32:26 -0800

    Add simple output of annotation api

--- main_api_server/server.js
+++ main_api_server/server.js
@@ -10,7 +10,29 @@ const app = express();
 app.get('/', function (req, res) {
   res.send(
   	{
-  		'message':'we come in p5c'
+  		'message':'OK'
+  	}
+  	);
+});
+
+app.get('/api/grade', function (req, res) {
+  res.send(
+  	{
+  		'grade':'0.8',
+  		'annotations': [
+  			{
+  				'id': '0',
+  				'x': '0.234',
+  				'y': '2.345',
+  				'label': 'hemorrhage'
+  			},
+  			{
+  				'id': '1',
+  				'x': '8.324',
+  				'y': '32.445',
+  				'label': 'exudate'
+  			},
+  		]
   	}
   	);
 });
