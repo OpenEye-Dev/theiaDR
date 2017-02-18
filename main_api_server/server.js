4209f356c71d2905cd5427344be8ffd0d2b530ed
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2017-02-18 09:53:20 -0800

    Add basic extra npm modules to server

--- main_api_server/server.js
+++ main_api_server/server.js
@@ -1,21 +1,54 @@
 'use strict';
+/*
+ * API SERVER 1
+----------------------------------------------------------------
+ * Author: Dhruv Joshi
+ * References:
+ * 1. https://scotch.io/tutorials/authenticate-a-node-js-api-with-json-web-tokens
+ * 2. 
+ */
 
 const express = require('express');
+
+// App
+const app = express();
+
+var bodyParser  = require('body-parser');
+var morgan      = require('morgan');
+var mongoose    = require('mongoose');
+var jwt         = require('jsonwebtoken'); // used to create, sign, and verify tokens
+// var config = require('./config'); // get our config file
+// var User   = require('./app/models/user'); // get our mongoose model
+
+// use body parser so we can get info from POST and/or URL parameters
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Constants
 const PORT = 8080;
 
-// App
-const app = express();
+// ROUTING
+// basic test route
 app.get('/', function (req, res) {
-  res.send(
-  	{
-  		'message':'OK'
-  	}
-  	);
+  res.send("This is where the login page would be");
 });
 
-app.get('/api/grade', function (req, res) {
+// If no route is matched by now, it must be a 404
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+ 
+
+// get an instance of the router for api routes
+var apiRoutes = express.Router();
+
+apiRoutes.get('/', function(req, res) {
+  res.json({ message: 'API OK' });
+});
+
+apiRoutes.post('/grade', function (req, res) {
   res.send(
   	{
   		'grade':'0.8',
@@ -36,6 +69,8 @@ app.get('/api/grade', function (req, res) {
   	}
   	);
 });
+
+app.use('/api', apiRoutes);
 
 app.listen(PORT);
 console.log('Running on http://localhost:' + PORT);
