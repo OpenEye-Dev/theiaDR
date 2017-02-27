207c818f506971e7c8cba2ce30d0386b9c431096
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2017-02-27 14:17:38 -0800

    Temporary pause to figure out tf
    Will be switching to another branch to figure out just reading in
    image files using pillow (python library) and creating a numpy
    array of numpy arrays (4D).

--- dlearn/train.py
+++ dlearn/train.py
@@ -6,14 +6,20 @@
    1000 randomly selected images from the Kaggle Diabetic Retinopathy training dataset
    were used to train a very basic classification model (which can be extended further)
 
+   TODO: Convert images to tfrecords format, and then read in using standard methods and train model. Follow along 
+   http://warmspringwinds.github.io/tensorflow/tf-slim/2016/12/21/tfrecords-guide/
+
 
    REFERENCES:
    * http://stackoverflow.com/questions/37340129/tensorflow-training-on-my-own-image
    * https://github.com/tensorflow/models/blob/master/tutorials/image/cifar10/cifar10_input.py
    * https://www.tensorflow.org/get_started/mnist/beginners
    * http://stackoverflow.com/questions/37126108/how-to-read-data-into-tensorflow-batches-from-example-queue
+   * http://stackoverflow.com/questions/39947512/load-image-files-in-a-directory-as-dataset-for-training-in-tensorflow
+   * http://stackoverflow.com/questions/36075319/how-do-you-load-label-and-feed-jpeg-data-into-tensorflow
 """
 import util
+import numpy, scipy.misc
 import tensorflow as tf
 
 # Constants
@@ -21,7 +27,16 @@ TRAINING_DATA_LOCATION = 'training_data/'
 IMAGE_SIZE = 512	# assume square image - this is side size in pixels
 BATCH_SIZE = 8
 IMAGE_CHANNELS = 3
+LABEL_LENGTH = 5
 
+UNROLLED_IMAGE_SIZE = IMAGE_SIZE*IMAGE_SIZE
+
+def label(n, LABEL_LENGTH=LABEL_LENGTH):
+	# returns a python list of length LABEL_LENGTH, with the n'th element 1 and rest 0
+	assert n < LABEL_LENGTH
+	l = [0]*LABEL_LENGTH
+	l[n] = 1
+	return l
 
 print 'Generating queue of images to be processed...'
 # Get training labels
@@ -30,7 +45,7 @@ tlabels = util.get_labels()
 # Generate training batch filebane pipeline
 # step 1: get filenames string list
 filenames = util.training_files(TRAINING_DATA_LOCATION)
-labels = [tlabels[x] for x in filenames]
+labels = [label(tlabels[x]) for x in filenames]
 
 # step 2: generate queue
 filename_queue = tf.train.string_input_producer(filenames)
@@ -43,16 +58,16 @@ image = tf.cast(image, tf.float32)
 resized_image = tf.image.resize_images(image, [IMAGE_SIZE, IMAGE_SIZE])
 
 # step 4: Batching
-image_batch, label_batch = tf.train.batch([resized_image, labels], batch_size=BATCH_SIZE)
+# image_batch, label_batch = tf.train.batch([resized_image, labels], batch_size=BATCH_SIZE)
 
 # Create the model
-x = tf.placeholder(tf.float32, [None, 784])
-W = tf.Variable(tf.zeros([784, 10]))
-b = tf.Variable(tf.zeros([10]))
+x = tf.placeholder(tf.float32, [None, UNROLLED_IMAGE_SIZE])
+W = tf.Variable(tf.zeros([UNROLLED_IMAGE_SIZE, LABEL_LENGTH]))
+b = tf.Variable(tf.zeros([LABEL_LENGTH]))
 y = tf.matmul(x, W) + b
 
 # Define loss and optimizer
-y_ = tf.placeholder(tf.float32, [None, 10])
+y_ = tf.placeholder(tf.float32, [None, LABEL_LENGTH])
 
 # The raw formulation of cross-entropy,
 #
@@ -71,6 +86,5 @@ sess = tf.InteractiveSession()
 tf.global_variables_initializer().run()
 
 # Train
-  for _ in range(len(filenames)):
-    batch_xs, batch_ys = tf.train.next_batch([resized_image, labels], batch_size=BATCH_SIZE)
-    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+for _ in range(len(filenames)):
+  sess.run(train_step, feed_dict={x: resized_image.eval(), y_: labels})
