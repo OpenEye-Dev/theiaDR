2d396d3fd4e49b8a48b1ddd93860bdeb9d573ae4
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2017-02-27 10:30:59 -0800

    Add basic tf file reading

--- dlearn/train.py
+++ dlearn/train.py
@@ -11,5 +11,30 @@
    * http://stackoverflow.com/questions/37340129/tensorflow-training-on-my-own-image
  """
  import util
- 
- # Start by reading in the training labels and putting values into a dict
+ import tensorflow as tf
+
+ # Constants
+ TRAINING_DATA_LOCATION = 'training_data/'
+ IMAGE_SIZE = 512	# assume square image - this is side size in pixels
+ BATCH_SIZE = 8
+ IMAGE_CHANNELS = 3
+
+ # Get training labels
+ tlabels = util.get_labels()
+
+# Generate training batch filebane pipeline
+# step 1: get filenames string list
+filenames = [x[0] for x in tlabels]
+
+# step 2: generate queue
+filename_queue = tf.train.string_input_producer(filenames)
+
+# step 3: read, decode and resize images
+reader = tf.WholeFileReader()
+filename, content = reader.read(filename_queue)
+image = tf.image.decode_jpeg(content, channels=IMAGE_CHANNELS)
+image = tf.cast(image, tf.float32)
+resized_image = tf.image.resize_images(image, [IMAGE_SIZE, IMAGE_SIZE])
+
+# step 4: Batching
+image_batch = tf.train.batch([resized_image], batch_size=BATCH_SIZE)
