f87e53c49f70e3d9e54262a6d2da040e1ebba029
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2017-02-27 10:15:58 -0800

    Add python files for tensorflow model training
    Util.py will have useful reusable functions. Train.py does the
    heavy lifting.

---
+++ dlearn/train.py
@@ -0,0 +1,15 @@
+ """
+   BASIC IMAGE CLASSIFICATION MODEL TRAINING IN TENSORFLOW
+   ---------------------------------------------------------------
+   AUTHOR: Dhruv Joshi
+
+   1000 randomly selected images from the Kaggle Diabetic Retinopathy training dataset
+   were used to train a very basic classification model (which can be extended further)
+
+
+   REFERENCES:
+   * http://stackoverflow.com/questions/37340129/tensorflow-training-on-my-own-image
+ """
+ import util
+ 
+ # Start by reading in the training labels and putting values into a dict
