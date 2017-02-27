c1ef651e7eead3a01eae8f37401ccbd048b3c506
Marco Montalto Monella <marco.montalto.93@gmail.com>
2017-02-26 23:05:55 -0800

    Add genContainer dev mode

--- genContainer.sh
+++ genContainer.sh
@@ -8,24 +8,42 @@ git clone ${GIT_URL}
 cd ${GIT_PROJECT}
 
 # Create a list of folders to display
-options=( $(find . -maxdepth 1 -type d) )
+GIT_DIRECTORIES=( $(find . -maxdepth 1 -type d) )
+
+
+# --------------------------------------------------
+# Defaults options
+DEV_MODE=false
+
+if [[ $# -gt 0 ]]
+then
+   for option in "$@";
+   do
+      if [[ $option == '-d' || $option == '--dev' ]]
+      then
+          DEV_MODE=true
+      fi
+   done
+fi
+
+# --------------------------------------------------
 
 # If module name missing or wrong, show a menu and ask for input.
-if [[ $# -eq 1  && " ${options[@]} " =~ " ./${1} " ]]
+if [[ " ${GIT_DIRECTORIES[@]} " =~ " ./${1} " ]]
     then
         MODULE=$1
     else
         while : ;
         do
             echo "Choose a module using its number:"
-            for ((i=1;i < ${#options[@]}; i++));
+            for ((i=1;i < ${#GIT_DIRECTORIES[@]}; i++));
             do
-                echo $i${options[i]}
+                echo $i${GIT_DIRECTORIES[i]}
             done
             read choice
-            if [[ $choice =~ ^[0-9]+$ && ${choice} -gt 0 && ${choice} -lt ${#options[@]} ]]
+            if [[ $choice =~ ^[0-9]+$ && ${choice} -gt 0 && ${choice} -lt ${#GIT_DIRECTORIES[@]} ]]
             then
-                MODULE=${options[$choice]/.\//}
+                MODULE=${GIT_DIRECTORIES[$choice]/.\//}
                 break
             fi
         done
@@ -34,10 +52,14 @@ fi
 
 DEV_USER=$(echo $USER | tr '[:upper:]' '[:lower:]')
 IMG_NAME=${DEV_USER}/${MODULE}
-IMG_TAG=v1
+IMG_TAG=0.1
 IMG_NAME+=":${IMG_TAG}"
 
 cd ${MODULE}
 docker build --build-arg module=${MODULE} -t ${IMG_NAME} .
-# TODO: launch bash only in dev-mode (bool arg)
-docker run --rm -ti ${IMG_NAME} bash
+
+if $DEV_MODE;
+then
+    docker run --rm -ti ${IMG_NAME} bash
+else
+    docker run --rm  ${IMG_NAME}
