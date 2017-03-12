c0e05744070c52a246e7475c625809aaca68deba
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2017-03-11 22:01:10 -0800

    Add simple code for reloading and testing model

---
+++ dlearn/test_saved_model.py
@@ -0,0 +1,27 @@
+# NOTE: Run this in the folder ~/retrain on the gpu1 VM
+
+image_data = tf.gfile.FastGFile('retina.jpg', 'rb').read()
+
+# A list of the actual labels
+label_lines = ["healthy", "unhealthy"]
+
+with tf.gfile.FastGFile("output_graph.pb", 'rb') as f:
+    graph_def = tf.GraphDef()
+    graph_def.ParseFromString(f.read())
+    _ = tf.import_graph_def(graph_def, name='')
+
+with tf.Session() as sess:
+    # Feed the image_data as input to the graph and get first prediction
+    softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+    
+    predictions = sess.run(softmax_tensor, \
+             {'DecodeJpeg/contents:0': image_data})
+    
+    # Sort to show labels of first prediction in order of confidence
+    top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
+    
+    for node_id in top_k:
+        human_string = label_lines[node_id]
+        score = predictions[0][node_id]
+        print('%s (score = %.5f)' % (human_string, score))
+
