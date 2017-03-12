191ba90ff61dd383e6eea1661d57ed13112ed404
Marco Montalto Monella <marco.montalto.93@gmail.com>
2017-03-12 16:38:08 -0700

    Add cluster.start script
    Create a kubernetes folder for all the yml files

---
+++ api_server/cluster.start.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# Switch to the kubernetes dir where the yml files are
+cd kubernetes
+
+# create cluster
+gcloud container clusters create cl1 --num-nodes=2
+
+# mongo and api server
+kubectl create -f db.yml -f web.yml
