c7bebb238d8848e90129f2af98ba89b65c83a21a
Marco Montalto Monella <marco.montalto.93@gmail.com>
2017-03-15 17:25:26 -0700

    Add routing to grade service
    Add sleep in cluster start script in order to make every pod
    initialize correctly.
    Add retina.jpg image for testing purposes

--- api_server/cluster.start.sh
+++ api_server/cluster.start.sh
@@ -6,5 +6,11 @@ cd kubernetes
 # create cluster
 gcloud container clusters create cl1 --num-nodes=2
 
-# mongo and api server
-kubectl create -f db.yml -f grade.yml -f web.yml
+# mongo, grade and api server
+# sleep are added in order to give time to the db to start
+sleep 1
+kubectl create -f db.yml
+sleep 5
+kubectl create -f grade.yml
+sleep 2
+kubectl create -f web.yml
