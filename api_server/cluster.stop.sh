3426c644d7a8997ceb5d2c6654b165c02c433477
Marco Montalto Monella <marco.montalto.93@gmail.com>
2017-03-19 12:21:35 -0700

    Clean up api_server scripts
    They have been included in control.sh.

--- api_server/cluster.stop.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-# Clean deploy, svc, rc and pods
-kubectl delete deploy --all
-kubectl delete svc --all
-kubectl delete rc --all
-kubectl delete pod --all
-
-gcloud container clusters delete cl1
