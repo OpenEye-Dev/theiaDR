6f33784d7124b02a4551125fbe9fc695fc088f55
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2017-03-21 15:06:22 -0700

    Add check for mongodb connection
    Send back an error message and 500 if the mongo connection is
    down.

--- api_server/controllers/authentication.js
+++ api_server/controllers/authentication.js
@@ -10,55 +10,65 @@ var sendJSONresponse = function(res, status, content) {
 };
 
 module.exports.register = function(req, res) {
-  // check if username or password are empty
-  if (req.body.username == '' || req.body.password == '') {
-    res.status(401).json({'message': 'username or password cannot be empty'});
-  } else if (req.body.signupCode == '' || req.body.signupCode == undefined) {
-    res.status(400).json({'message':'signup code missing'});
-  } else {
-      User.findOne({ username: req.body.username }, function (err, user) {
-      if (err) { res.send(err); }
-      // Return if user not found in database
-      if (user) {
-        res.json({
-          message: 'User already exists'
-        });
-      } else {
-        // check if signupCode is correct
-        if (SIGNUP_CODES.indexOf(req.body.signupCode) != -1) {
-          var user = new User();
-          user.username = req.body.username;
-          user.setPassword(req.body.password);
-          user.save(function(err) {
-            res.status(200);
-            res.json(user.generateJwt());
+  // first check if the DB connection is still alive
+  if (mongoose.connection.readyState == 1) {
+    // check if username or password are empty
+    if (req.body.username == '' || req.body.password == '') {
+      res.status(401).json({'message': 'username or password cannot be empty'});
+    } else if (req.body.signupCode == '' || req.body.signupCode == undefined) {
+      res.status(400).json({'message':'signup code missing'});
+    } else {
+        User.findOne({ username: req.body.username }, function (err, user) {
+        if (err) { res.send(err); }
+        // Return if user not found in database
+        if (user) {
+          res.json({
+            message: 'User already exists'
           });
         } else {
-          res.status(400).json({'message':'incorrect signup code'});
+          // check if signupCode is correct
+          if (SIGNUP_CODES.indexOf(req.body.signupCode) != -1) {
+            var user = new User();
+            user.username = req.body.username;
+            user.setPassword(req.body.password);
+            user.save(function(err) {
+              res.status(200);
+              res.json(user.generateJwt());
+            });
+          } else {
+            res.status(400).json({'message':'incorrect signup code'});
+          }
         }
-      }
-    });
+      });
+    }    
+  } else {
+    res.status(500).json({'message': 'Sorry, something went wrong.'})
   }
+
 };
 
 module.exports.login = function(req, res) {
-  passport.authenticate('local', function(err, user, info){
-    var token;
+  // first check if the DB connection is still alive
+  if (mongoose.connection.readyState == 1) {
+    passport.authenticate('local', function(err, user, info) {
+      var token;
 
-    // If Passport throws/catches an error
-    if (err) {
-      res.status(404).json(err);
-      return;
-    }
+      // If Passport throws/catches an error
+      if (err) {
+        res.status(404).json(err);
+        return;
+      }
 
-    // If a user is found
-    if(user){
-      res.status(200);
-      res.json(user.generateJwt());
-    } else {
-      // If user is not found
-      res.status(401).json(info);
-    }
-  })(req, res);
-
+      // If a user is found
+      if (user) {
+        res.status(200);
+        res.json(user.generateJwt());
+      } else {
+        // If user is not found
+        res.status(401).json(info);
+      }
+    })(req, res);
+  } else {
+    res.status(500).json({'message': 'Sorry, something went wrong.'})
+  }
 };
