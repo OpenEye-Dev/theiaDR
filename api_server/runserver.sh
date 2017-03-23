0368de27f55bd7887be11fee0d3454be7531874f
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2017-03-23 11:33:36 -0700

    Add development mode to testing app.py server
    If the runserver.sh is run in the nodejs container with the arg
    'dev' then it assumes that you want to develop app.py and therefore
    will assume there is only one tf-server container (onto which you
    are going to be mount the app.py) and thus will set both env
    variables (flower and eye) to point to this one grading container.

--- api_server/runserver.sh
+++ api_server/runserver.sh
@@ -4,10 +4,21 @@ echo "setting environment variables for the mongodb and the tensorflow server"
 # first mongo
 export DB_SERVICE_HOST=$(cat /etc/hosts | grep mongo | awk {'print $1'})
 
-# tf-models
-export GRADEEYE_SERVICE_HOST=$(cat /etc/hosts | grep tf-model-eye | awk {'print $1'})
+if [ $1 ] && [ $1 = "dev" ]; then
+	# tf-models - set all the GRADING server IP addresses to be the same single tf-server container
+	echo "Development mode. Setting all grading services to be the single tf-server"
+	export GRADEEYE_SERVICE_HOST=$(cat /etc/hosts | grep tf-server | awk {'print $1'})
+	export GRADEFLOWER_SERVICE_HOST=$(cat /etc/hosts | grep tf-server | awk {'print $1'})
+
+else
+	# tf-models
+	echo "Normal mode. There are multiple grading services (flower and eye)"
+	export GRADEEYE_SERVICE_HOST=$(cat /etc/hosts | grep tf-model-eye | awk {'print $1'})
+	export GRADEFLOWER_SERVICE_HOST=$(cat /etc/hosts | grep tf-model-flower | awk {'print $1'})
+fi
+
+# all ports are 8080 anyway
 export GRADEEYE_SERVICE_PORT=8080
-export GRADEFLOWER_SERVICE_HOST=$(cat /etc/hosts | grep tf-model-flower | awk {'print $1'})
 export GRADEFLOWER_SERVICE_PORT=8080
 
 # starting application
