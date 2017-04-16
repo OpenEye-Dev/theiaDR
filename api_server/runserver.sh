486cf6ea4f794d94e6d8a3ea000c8cfda1ad8aa3
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2017-04-16 14:36:36 -0700

    Update runserver
    The shell script will now activate a singular grading service
    environment variable (simulating development mode) and will
    check if node_modules are installed and do an npm install if not.

--- api_server/runserver.sh
+++ api_server/runserver.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
-echo "setting environment variables for the mongodb and the tensorflow server"
+echo "setting environment variables for the grading server"
 
 # first mongo
 export DB_SERVICE_HOST=$(cat /etc/hosts | grep mongo | awk {'print $1'})
 
-if [ $1 ] && [ $1 = "dev" ]; then
+if [ $1 ] && [ $1 = "prod" ]; then
 	# tf-models - set all the GRADING server IP addresses to be the same single tf-server container
-	echo "Development mode. Setting all grading services to be the single tf-server"
-	export GRADEEYE_SERVICE_HOST=$(cat /etc/hosts | grep tf-server | awk {'print $1'})
-	export GRADEFLOWER_SERVICE_HOST=$(cat /etc/hosts | grep tf-server | awk {'print $1'})
+	echo "Simulation of production mode. Multiple models activated"
+	# TODO
 
 else
-	# tf-models
-	echo "Normal mode. There are multiple grading services (flower and eye)"
-	export GRADEEYE_SERVICE_HOST=$(cat /etc/hosts | grep tf-model-eye | awk {'print $1'})
-	export GRADEFLOWER_SERVICE_HOST=$(cat /etc/hosts | grep tf-model-flower | awk {'print $1'})
+	# Standard - one model
+	echo "Activating development mode.."
+	# TODO
+	export GRADEDR_SERVICE_HOST=$(cat /etc/hosts | grep torch-server | awk {'print $1'})
 fi
 
-# all ports are 8080 anyway
-export GRADEEYE_SERVICE_PORT=8080
-export GRADEFLOWER_SERVICE_PORT=8080
+# all ports are 8910
+export GRADEDR_SERVICE_PORT=8910
 
 # starting application
+if [ ! -d node_modules ]; then
+    echo "Installing nodejs modules..."
+    npm install
+fi
 nodemon
