f63152ff25a15c7586c02de44950dc9a98ceae06
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2017-04-18 20:08:56 -0700

    Change torch server to background port 8080

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 # Starts all the containers and mounts the api_server directory onto the api_server container
 # gives bash access to it
 docker run --name mongo -d --rm mongo mongod
-docker run --name torch-server -d --rm -p 8910:8910 tswedish/actdr
+docker run --name torch-server -d --rm tswedish/actdr
 
 cd api_server
 docker run --rm -it --link mongo:mongo --link torch-server -p 8080:8080 -v `pwd`:/src --name api-server cs193s/api_server_dev bash
