38868e6be9712238cbfae8c46947d2c24064f4dc
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2017-04-18 22:00:41 -0700

    Add more clear instructions to console

--- start.sh
+++ start.sh
@@ -8,4 +8,6 @@ echo 'starting torch container...'
 docker run --name torch-server -d --rm tswedish/actdr
 
 cd api_server
+echo 'starting main API Server container. You will be given BASH access. Please run the following command to start the server:'
+echo 'source runserver.sh'
 docker run --rm -it --link mongo:mongo --link torch-server -p 8080:8080 -v `pwd`:/src --name api-server cs193s/api_server_dev bash
